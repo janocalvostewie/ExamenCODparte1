bed22a6f148b2d0a7e867e45ba81ec76c8bb1100
Jano <Jano@Janillo>
2016-06-06 08:40:47 +0200

    Commit 1: Renombrado de un método

--- src/examenfinalparte1/Principal.java
+++ src/examenfinalparte1/Principal.java
@@ -1,9 +1,28 @@
 package examenfinalparte1;
+
 public class Principal {
+
     int i;
-public Principal(int j, int m) {i=met(j,m);}
+
+    public Principal(int j, int m) {
+        i = sumar(j, m);
+    }
+
     public Principal(int j) {
-        i=met(j,j);}
-    public Principal() { i=-1;
-    }public int met(int x, int x1) {
-      return x + x1;}}
+        i = sumar(j, j);
+    }
+
+    public Principal() {
+        i = -1;
+    }
+
+    /**
+     * Método que suma las dos variables que se pasan como parámetros
+     * @param x variable integer que se sumará
+     * @param x1 variable integer que se sumará
+     * @return 
+     */
+    public int sumar(int x, int x1) {
+        return x + x1;
+    }
+}
